3cc8a9b2f0c42a76243fb0a173ea31a75d97ac77
Kentendo <kenny@kennycosca.com>
2015-05-07 23:58:56 -0700

    switching json url back to original

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 angular.module('learningfuze.config', [])
 .constant('LessonConfig', {
   'editorTheme': 'ace/theme/eclipse',
-  'lessonUrl': 'sequence_sample.json.txt',
+  'lessonUrl': 'https://gist.githubusercontent.com/thireven/841b585de43a7d708bff/raw/abe8f8ceacff3817fb3ee5bf8219408819563629/sequence_sample.json',
   'defaultSet': 0,
   'defaultOutput': 'Choose the Intro task from a sequence dropdown above.',
   'taskCompleteMessage': 'You did it!',
